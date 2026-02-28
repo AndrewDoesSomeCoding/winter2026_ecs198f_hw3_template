8a9e7fb7dc9dbdca36c8be4033d0fe064f9459bb
Andrew Gutierrez <argutierrez2006@gmail.com>
2026-02-27 21:13:04 -0800

    Basic tests

--- test_foo_bar_baz.py
+++ test_foo_bar_baz.py
@@ -3,3 +3,19 @@ import pytest
 from foo_bar_baz import foo_bar_baz
 
 #Add testcases Here
+
+def test_basic_inputs():
+    from foo_bar_baz import foo_bar_baz
+    
+    assert foo_bar_baz(0) == ''
+    
+    assert foo_bar_baz(1) == '1'
+    
+    assert foo_bar_baz(3) == '1 2 Foo'
+    
+    assert foo_bar_baz(5) == 'Bar'
+
+def test_less_basic():
+    pass
+    
+
