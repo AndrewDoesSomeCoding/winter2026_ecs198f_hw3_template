194cae0cb7bf35365f802bf7bb3fdaa1c199de1c
Andrew Gutierrez <argutierrez2006@gmail.com>
2026-02-27 22:25:27 -0800

    Added a few more tests

--- foo_bar_baz.py
+++ foo_bar_baz.py
@@ -2,13 +2,18 @@ def foo_bar_baz(n: int) -> str:
     return_str = ""
     for i in range(1, n + 1, 1):
         if (i % 3 == 0) and (i % 5 == 0):
+            print(return_str)
             return_str += "Baz"
         elif i % 3 == 0:
+            print(return_str)
             return_str += "Foo"
         elif i % 5 == 0:
+            print(return_str)
             return_str += "Bar"
         else:
+            print(return_str)
             return_str += str(i)
         if i < n:
             return_str += " "
+    print(return_str)
     return return_str
--- test_foo_bar_baz.py
+++ test_foo_bar_baz.py
@@ -2,8 +2,6 @@ import pytest
 
 from foo_bar_baz import foo_bar_baz
 
-#Add testcases Here
-
 def test_basic_inputs():
     from foo_bar_baz import foo_bar_baz
     
@@ -17,11 +15,43 @@ def test_basic_inputs():
     
     assert foo_bar_baz(15) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz"
 
-def test_less_basic():
-    pass
-
 def test_negative_numbers():
     negatives = [-1, -10, -1000, -1000000, -10000000]
     for n in negatives:
         assert foo_bar_baz(n) == ""
+
+def test_format_string_15():
+    n = 15
+    result = foo_bar_baz(n)
+
+    assert result[0] != " "
+    assert result[-1] != " "
+
+    for i in range(len(result) - 1):
+        assert not (result[i] == " " and result[i + 1] == " ")
+
+    space_count = 0
+    for ch in result:
+        if ch == " ":
+            space_count += 1
+
+    assert space_count == n - 1
         
+        
+def test_bad_arguments():
+    from foo_bar_baz import foo_bar_baz
+
+    with pytest.raises(TypeError):
+        foo_bar_baz(6.7) 
+
+    with pytest.raises(TypeError):
+        foo_bar_baz("") 
+    
+    with pytest.raises(TypeError):
+        foo_bar_baz(bool)
+        
+    with pytest.raises(TypeError):
+        foo_bar_baz(bytes)
+        
+    with pytest.raises(TypeError):
+        foo_bar_baz(list)
