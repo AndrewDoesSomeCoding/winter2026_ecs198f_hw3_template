999bd2cacece15eefc4150634ee13f495bb5e334
Andrew Gutierrez <argutierrez2006@gmail.com>
2026-02-27 21:30:19 -0800

    Added negative tests

--- test_foo_bar_baz.py
+++ test_foo_bar_baz.py
@@ -14,8 +14,14 @@ def test_basic_inputs():
     assert foo_bar_baz(3) == '1 2 Foo'
     
     assert foo_bar_baz(5) == '1 2 Foo 4 Bar'
+    
+    assert foo_bar_baz(15) == "1 2 Foo 4 Bar Foo 7 8 Foo Bar 11 Foo 13 14 Baz"
 
 def test_less_basic():
     pass
-    
 
+def test_negative_numbers():
+    negatives = [-1, -10, -1000, -1000000, -10000000]
+    for n in negatives:
+        assert foo_bar_baz(n) == ""
+        
